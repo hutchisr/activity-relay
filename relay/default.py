1b169b29c20751f1c01f31e758bc5e4ad62c0c5e
Rachel <rachel@homunyan.com>
2018-12-05 07:25:28 +0000

    Cleanup

--- relay/default.py
+++ relay/default.py
@@ -1,5 +1,5 @@
 import aiohttp.web
-import urllib.parse
+from urllib.parse import urlsplit
 from . import app, CONFIG
 from .database import DATABASE
 
@@ -9,7 +9,10 @@ note = CONFIG['note']
 inboxes = DATABASE.get('relay-list', [])
 
 async def default(request):
-    targets = '<br>'.join([urllib.parse.urlsplit(target).hostname for target in inboxes])
+    targets = ''
+    for target in inboxes:
+        parsed = urlsplit(target)
+        targets += '<li><a href="{}://{}">{}</a></li>'.format(parsed.scheme, parsed.netloc, parsed.hostname)
     return aiohttp.web.Response(
         status=200,
         content_type="text/html",
@@ -18,17 +21,23 @@ async def default(request):
 <html><head>
  <title>ActivityPub Relay at {host}</title>
  <style>
-  p {{ color: #FFFFFF; font-family: monospace, arial; font-size: 100%; }}
-  body {{ background-color: #000000; }}
+  {{ color: #9baec8; font-family: Roboto, sans-serif; font-size: 16px; font-weight: 400;}}
+  body {{ background-color: #16191f; color: #9baec8; font-family: Roboto, sans-serif; font-size: 16px; font-weight: 400;}}
+  a {{ color: #2b90d9; text-decoration: none; }}
+  a:hover {{ text-decoration: underline; }}
   </style>
 </head>
 <body>
-<p>This is an Activity Relay for fediverse instances.</p>
+<h1>This is an Activity Relay for fediverse instances.</h1>
+<section>
 <p>{note}</p>
 <p>For Mastodon instances, you may subscribe to this relay with the address: <a href="https://{host}/inbox">https://{host}/inbox</a></p>
 <p>For Pleroma and other instances, you may subscribe to this relay with the address: <a href="https://{host}/actor">https://{host}/actor</a></p>
 <p>To host your own relay, you may download the code at this address: <a href="https://git.pleroma.social/pleroma/relay">https://git.pleroma.social/pleroma/relay</a></p>
-<br><p>List of {count} registered instances:<br>{targets}</p>
+</section><section>
+<h4>{count} registered instances</h4>
+<ul>{targets}</ul>
+</section>
 </body></html>
 
 """.format(host=host, note=note,targets=targets,count=len(inboxes)))
