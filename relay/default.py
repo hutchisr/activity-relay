b8a492402f64870a337cf77dc4ac419cec0f460f
Rachel <rachel@homunyan.com>
2022-11-02 06:03:04 +0000

    Upgrade

--- relay/default.py
+++ relay/default.py
@@ -26,7 +26,7 @@ async def default(request):
   a {{ color: #2b90d9; text-decoration: none; }}
   a:hover {{ text-decoration: underline; }}
   @media only screen and (min-width : 1025px) {{
-    ul {{ display: flex; flex-flow: wrap column; max-height: 500px; }}
+    ul.instances {{ column-count: 4; }}
   }}
   </style>
 </head>
@@ -37,12 +37,21 @@ async def default(request):
 <p>For Mastodon instances, you may subscribe to this relay with the address: <a href="https://{host}/inbox">https://{host}/inbox</a></p>
 <p>For Pleroma and other instances, you may subscribe to this relay with the address: <a href="https://{host}/actor">https://{host}/actor</a></p>
 <p>To host your own relay, you may download the code at this address: <a href="https://git.pleroma.social/pleroma/relay">https://git.pleroma.social/pleroma/relay</a></p>
-<p>
-To report abuse please contact <a href="https://social.homunyan.com/@animeirl">@animeirl</a>
-</p>
+<h4>Disclaimer</h4>
+<p>The servers below are not endorsed by the owners of this relay nor are they actively monitored.</p>
+<p>To report abuse please contact <a href="https://social.homunyan.com/@animeirl">@animeirl</a></p>
+<h4>Donations</h4>
+<p>This server costs time and money to maintain. Donations are accepted.</p>
+<ul>
+<li>XLM: <code class="fake-money">GBQAJJAOGIUY6N355D4EPIMNDGJSEJVOALFPQ2BHIF5WAHVBPGTIFBCJ</code></li>
+<li>BTC: <code class="fake-money">bc1qegdtds7jtqd82rfnst783543tztpeljtqkjnul</code></li>
+<li>ETH: <code class="fake-money">0x343eB67F6e7d00d88Bd2a033eb90ff7264698879</code></li>
+<li>XMR: <code class="fake-money">89TNsVHs1pChcRUjsrB355hTuhiMCYgQqhchUVioHLikBqn3XrAqAfmZZxmD6g1K66XzbxRw1jnGCZry8xep9FQh1UiBpdp</code></li>
+<li>ADA: <code class="fake-money">addr1qx4z9qtfnpg5duefmc03lgq0u9xg7axrukwypxya4nl92kpntyu6p952szu80scwuxq6vmy8rzfa2yq2xw5cqqu42rgsr888sm</code></li>
+</ul>
 </section><section>
 <h4>{count} registered instances</h4>
-<ul>{targets}</ul>
+<ul class="instances">{targets}</ul>
 </section>
 </body></html>
 
